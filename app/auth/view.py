190ef1874dffc455926af93e928ae5d7b1883486
ClintonClin <clintonclin00@gmail.com>
2018-11-28 08:30:31 +0300

    edited my errors

--- app/auth/view.py
+++ app/auth/view.py
@@ -19,7 +19,7 @@ def login():
 
         flash('Invalid username or password')
 
-    title = "Pitch login"
+    title = "Blog login"
     return render_template('auth/login.html', login_form = login_form, title=title)  # login view function that renders a template file.
 
 @auth.route('/register', methods  = ["GET", "POST"])
